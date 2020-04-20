a17e87af56728ad30aad6ba6ed9583897207d935
Lachlan Mackenzie <36923157+lacmac@users.noreply.github.com>
2020-04-20 22:27:32 +0800

    Searching for shows in Plex

--- tvmaze.bundle/Contents/Code/__init__.py
+++ tvmaze.bundle/Contents/Code/__init__.py
@@ -7,7 +7,8 @@ SUPPORTED_LANGUAGES = [
     'ab', 'sq', 'ar', 'hy', 'az', 'bg', 'zh', 'cs', 'da', 'nl', 'en', 'et',
     'fi', 'fr', 'ka', 'de', 'ga', 'he', 'hi', 'hr', 'hu', 'is', 'it', 'ja',
     'ko', 'no', 'fa', 'pl', 'pt', 'ro', 'ru', 'sr', 'es', 'sv', 'tl', 'th',
-    'tr', 'uk', 'ur', 'cy']
+    'tr', 'uk', 'ur', 'cy'
+]
 
 
 def Start():
@@ -18,12 +19,37 @@ class TVmazeAgent(Agent.TV_Shows):
     name = "TVmaze"
     languages = SUPPORTED_LANGUAGES
     primary_provider = True
-    fallback_agent = False
-    accepts_from = None
-    contributes_to = None
 
     def search(self, results, media, lang, manual):
-        pass
+        Log.Info('Searching for matches with TVmaze agent: ' + media.show)
+        # Retrieve list of shows that match the given name.
+        shows = tvmaze.search_show(media.show)
+
+        Log.Info('Found {} shows that match {}'.format(len(shows), media.show))
+        scores = [show.score for show in shows]
+        for show in shows:
+            # Retrieve language of the show
+            locale = Locale.Language.Match(show.language) if show.language is None else lang
+
+            # Scale the scores so the best match has a score of 100.
+            scale = (show.score - min(scores)) / (max(scores) - min(scores))
+            scaled_score = int(99 * scale + 1) if len(shows) > 1 else 100
+
+            # Add the show to the return list
+            results.Append(MetadataSearchResult(
+                id=str(show.id),
+                name=show.name,
+                year=int(show.premiere_date[:4]) if show.premiere_date is not None else None,
+                score=scaled_score,
+                lang=locale,
+            ))
+            Log.Debug('Added result MetadataSearchResult(id="{}", name="{}", score="{}", year="{}", lang="{}")'.format(
+                str(show.id),
+                show.name,
+                str(scaled_score),
+                show.premiere_date[:4] if show.premiere_date is not None else '',
+                locale,
+            ))
 
     def update(self, metadata, media, lang, force):
         pass
