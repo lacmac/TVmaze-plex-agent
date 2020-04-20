7c751e82addb7b110b55e644f034ebe8481f387c
Lachlan Mackenzie <36923157+lacmac@users.noreply.github.com>
2020-04-20 22:47:04 +0800

    Improved logging

--- tvmaze.bundle/Contents/Code/__init__.py
+++ tvmaze.bundle/Contents/Code/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import timeit
+
 from tvmazepy import tvmaze
 
 
@@ -15,25 +17,29 @@ def Start():
     Log.Debug('Starting TVmaze agent...')
 
 
+def time_taken(start, end):
+    return '{:0.4f}'.format(end-start)
+
+
 class TVmazeAgent(Agent.TV_Shows):
     name = "TVmaze"
     languages = SUPPORTED_LANGUAGES
     primary_provider = True
 
     def search(self, results, media, lang, manual):
+        start = timeit.default_timer()
         Log.Info('Searching for matches with TVmaze agent: ' + media.show)
         # Retrieve list of shows that match the given name.
         shows = tvmaze.search_show(media.show)
 
-        Log.Info('Found {} shows that match {}'.format(len(shows), media.show))
+        Log.Info('Found {} shows that match "{}"'.format(len(shows), media.show))
         scores = [show.score for show in shows]
         for show in shows:
             # Retrieve language of the show
             locale = Locale.Language.Match(show.language) if show.language is None else lang
 
             # Scale the scores so the best match has a score of 100.
-            scale = (show.score - min(scores)) / (max(scores) - min(scores))
-            scaled_score = int(99 * scale + 1) if len(shows) > 1 else 100
+            scaled_score = int(99 * (show.score - min(scores)) / (max(scores) - min(scores)) + 1) if len(shows) > 1 else 100
 
             # Add the show to the return list
             results.Append(MetadataSearchResult(
@@ -43,13 +49,15 @@ class TVmazeAgent(Agent.TV_Shows):
                 score=scaled_score,
                 lang=locale,
             ))
-            Log.Debug('Added result MetadataSearchResult(id="{}", name="{}", score="{}", year="{}", lang="{}")'.format(
+            Log.Info('Added result MetadataSearchResult(id="{}", name="{}", score="{}", year="{}", lang="{}")'.format(
                 str(show.id),
                 show.name,
                 str(scaled_score),
                 show.premiere_date[:4] if show.premiere_date is not None else '',
                 locale,
             ))
+        end = timeit.default_timer()
+        Log.Info('Search completed with {} matches in {} seconds'.format(len(shows), time_taken(start, end)))
 
     def update(self, metadata, media, lang, force):
         pass
